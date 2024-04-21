e567501101bff1d408b617cc6e822b6918861741
Manahil rasheed <manahilrasheed507@gmail.com>
2024-04-21 07:00:07 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// string concatenation
+var firstName = "Manahil";
+var lastName = "Rasheed";
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+// template literals
+var message = "i am 20 years";
+var message2 = "old";
+var cart = "".concat(message, " ").concat(message2);
+console.log(cart);
---
+++ main.ts
@@ -0,0 +1,14 @@
+// string concatenation
+
+let firstName : string = "Manahil"
+let lastName: string = "Rasheed"
+let fullName: string = firstName + ' ' + lastName
+console.log(fullName)
+
+
+// template literals
+
+let message: string = "i am 20 years"
+let message2: string = "old"
+let cart: string = `${message} ${message2}`;
+console.log(cart)
